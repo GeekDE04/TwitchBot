9fb61d75d60a3e814ae45a6c9c08a1856f88775e
GeekDE04 <67628104+GeekDE04@users.noreply.github.com>
2020-06-30 09:50:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const tmi = require("tmi.js");
+
+const options = {
+  options: {
+    debug: true,
+  },
+  connection: {
+    cluster: 'aws',
+    reconnect: true,
+  },
+  identity: {
+    username: 'botname',
+    password: 'outh:password',
+  },
+  channels: ['channelname'],
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on('connected', (adress, port) => {
+  client.action('geekde04','Hallo, Bot is now connected');
+});
+
+client.on('chat', (channel, user, message, self) => {
+  if (message === '!boti') {
+    client.action('channelname','Botinfo');
+  }
+});
